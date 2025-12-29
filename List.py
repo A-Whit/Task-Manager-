c2c36126cb7e11ea1da2c501c4b441e440114344
Zo <alonzo.whitaker12@gmail.com>
2025-12-28 20:44:52 -0500

    Add files via upload

---
+++ List.py
@@ -0,0 +1,13 @@
+# Here is where I am creating my list and task for my todo list. to keep it simple I am only going to do 5 
+
+#task
+({'babysit', 'chores', 'wash_clothes', ' get_groceries', 'relax'})
+
+
+#list
+kids = ( ["tommy", "chuckie" , "phill", "lil" "susy" ])
+house = ([ 'sweep', 'mop', 'trash'])
+laundry = (['sort_clothes', 'run_water', 'add_detergant', 'start' ])
+groceries = (['milk', 'eggs', 'beer', ])
+relax = (['beer', 'beer' , 'beer'])
+
